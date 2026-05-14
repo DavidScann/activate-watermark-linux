41256b6a3ae50068d6107b2592ae30aca32789bd
David Nguyen <davidscann@saltcute.moe>
2026-05-14 14:31:35 +0700

    feat: add WatermarkWindow overlay with paint/position/timer

---
+++ src/WatermarkWindow.cpp
@@ -0,0 +1,103 @@
+#include "WatermarkWindow.h"
+#include <QPainter>
+#include <QFontMetrics>
+#include <QScreen>
+#include <QGuiApplication>
+#include <QFontDatabase>
+
+WatermarkWindow::WatermarkWindow(QWidget *parent)
+    : QWidget(parent)
+{
+    setWindowFlags(Qt::FramelessWindowHint
+                   | Qt::WindowStaysOnTopHint
+                   | Qt::Tool
+                   | Qt::X11BypassWindowManagerHint);
+    setAttribute(Qt::WA_TranslucentBackground);
+    setAttribute(Qt::WA_TransparentForMouseEvents);
+    setAttribute(Qt::WA_ShowWithoutActivating);
+    setAttribute(Qt::WA_X11DoNotPropagateOpacity);
+
+    QFont sysFont = QFontDatabase::systemFont(QFontDatabase::GeneralFont);
+    m_line1Font = sysFont;
+    m_line2Font = sysFont;
+
+    connect(&m_refreshTimer, &QTimer::timeout, this, &WatermarkWindow::updatePosition);
+    m_refreshTimer.setInterval(5000);
+    m_refreshTimer.start();
+}
+
+void WatermarkWindow::rebuildFonts()
+{
+    m_line1Font.setPixelSize(m_settings.fontSize1);
+    m_line2Font.setPixelSize(m_settings.fontSize2);
+}
+
+void WatermarkWindow::applySettings(const Settings &settings)
+{
+    m_settings = settings;
+    rebuildFonts();
+    updatePosition();
+    update();
+}
+
+void WatermarkWindow::updatePosition()
+{
+    QScreen *screen = QGuiApplication::primaryScreen();
+    if (!screen) return;
+
+    QRect avail = screen->availableGeometry();
+    const int margin = 20;
+
+    QFontMetrics fm1(m_line1Font);
+    QFontMetrics fm2(m_line2Font);
+    QRect bounds1 = fm1.boundingRect(m_settings.line1);
+    QRect bounds2 = fm2.boundingRect(m_settings.line2);
+
+    int lineSpacing = 2;
+    int maxW = qMax(bounds1.width(), bounds2.width());
+    int totalH = bounds1.height() + lineSpacing + bounds2.height();
+    int winW = maxW + 8;
+    int winH = totalH + 4;
+
+    int x, y;
+    switch (m_settings.position) {
+    case 0: // bottom-right
+        x = avail.right() - winW - margin;
+        y = avail.bottom() - winH - margin;
+        break;
+    case 1: // bottom-left
+        x = avail.left() + margin;
+        y = avail.bottom() - winH - margin;
+        break;
+    case 2: // top-right
+        x = avail.right() - winW - margin;
+        y = avail.top() + margin;
+        break;
+    case 3: // top-left
+    default:
+        x = avail.left() + margin;
+        y = avail.top() + margin;
+        break;
+    }
+
+    setGeometry(x, y, winW, winH);
+}
+
+void WatermarkWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setRenderHint(QPainter::TextAntialiasing);
+
+    QColor textColor(255, 255, 255, m_settings.alpha);
+
+    painter.setFont(m_line1Font);
+    painter.setPen(textColor);
+    QRectF line1Rect = painter.fontMetrics().boundingRect(m_settings.line1);
+    painter.drawText(0, line1Rect.height(), m_settings.line1);
+
+    painter.setFont(m_line2Font);
+    int line2Y = line1Rect.height() + 2;
+    QRectF line2Rect = painter.fontMetrics().boundingRect(m_settings.line2);
+    painter.drawText(0, line2Y + line2Rect.height(), m_settings.line2);
+}
---
+++ src/WatermarkWindow.h
@@ -0,0 +1,28 @@
+#ifndef WATERMARKWINDOW_H
+#define WATERMARKWINDOW_H
+
+#include <QWidget>
+#include <QTimer>
+#include "Settings.h"
+
+class WatermarkWindow : public QWidget {
+    Q_OBJECT
+public:
+    explicit WatermarkWindow(QWidget *parent = nullptr);
+
+    void applySettings(const Settings &settings);
+    void updatePosition();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    Settings m_settings;
+    QTimer m_refreshTimer;
+    QFont m_line1Font;
+    QFont m_line2Font;
+
+    void rebuildFonts();
+};
+
+#endif // WATERMARKWINDOW_H
